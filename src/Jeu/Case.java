069b14020cc4960adfc2d623c1e62c1cbab2ac7f
BARBOBZ <jordanbarbosa63112@gmail.com>
2025-11-18 14:01:38 +0100

    1er Push test

---
+++ src/Jeu/Case.java
@@ -0,0 +1,16 @@
+package Jeu;
+
+public class Case {
+
+    private int numCase;
+
+    public Case(int numCase)
+    {
+        this.numCase = numCase;
+    }
+
+    public int getNum(){
+        return numCase;
+    }
+
+}
---
+++ src/Jeu/Plateau.java
@@ -0,0 +1,17 @@
+package Jeu;
+
+public class Plateau {
+
+    private static final int NB_CASES = 63;
+    private final Case[] cases;
+
+    public Plateau() {
+
+        cases = new Case[NB_CASES];
+        for (int i = 0; i <= NB_CASES; i++) {
+
+            cases[i] = new Case(i);
+        }
+
+    }
+}
